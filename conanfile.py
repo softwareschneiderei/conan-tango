d5379c48c733369d938b380ef4a539860b34f1d6
Marius Elvert <elvert@softwareschneiderei.de>
2019-07-30 16:07:17 +0200

    Corrected install step

--- conanfile.py
+++ conanfile.py
@@ -38,15 +38,10 @@ class TangoConan(ConanFile):
                     installer.install(package)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.install()
 
     def package_info(self):
-        #self.cpp_info.libs = ["hello"]
-        pass
+        self.cpp_info.libs = ["tango"]
 
 
