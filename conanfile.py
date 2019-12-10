727304f6aedb04c3abb0e08388939cf24d0aa257
Marius Elvert <marius.elvert@softwareschneiderei.de>
2019-12-10 15:10:52 +0100

    Properly enhance files, even without sed

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,7 @@ from shutil import copyfile
 from conans import ConanFile, tools, CMake
 from conans.errors import ConanException, ConanInvalidConfiguration
 
-SOURCE_ARCHIVE = "tango-9.2.5a.tar.gz"
-TANGO_CONFIG_FOR_GCC49_PATCH = "tango_config_for_gcc49.patch"
+NO_SED_PATCH = "0001-Do-not-use-sed-for-file-enhancements.patch"
 
 
 def replace_prefix_everywhere_in_pc_file(file, prefix):
@@ -28,7 +27,7 @@ class TangoConan(ConanFile):
     generators = "cmake"
     file_prefix = "{0}-{1}".format(name, version)
     source_archive = "{0}.tar.gz".format(file_prefix)
-    exports_sources = source_archive, TANGO_CONFIG_FOR_GCC49_PATCH
+    exports_sources = NO_SED_PATCH,
     requires = "zlib/1.2.11@conan/stable", "zmq/4.3.1@bincrafters/stable",\
                "cppzmq/4.4.1@bincrafters/stable", "omniorb/4.2.2@None/None"
 
@@ -45,14 +44,6 @@ class TangoConan(ConanFile):
             raise ConanInvalidConfiguration("Conan needs the setting 'compiler.libcxx' to be 'libstdc++11' on linux")
 
     def _configured_cmake(self):
-        source_location = os.path.join(self.source_folder, "cppTango")
-        idl_location = os.path.join(self.source_folder, "tango-idl")
-        os.makedirs("tango-idl/include", exist_ok=True)
-        shutil.copy(os.path.join(idl_location, "tango.idl"), os.path.join(self.build_folder, "tango-idl/include/"))
-
-        # conan seems to only support in-source builds right now
-        shutil.copytree(source_location, self.build_folder, dirs_exist_ok=True)
-
         cmake = CMake(self)
         with tools.environment_append({"OMNI_BASE": self.deps_cpp_info["omniorb"].rootpath}):
             cmake.configure(
@@ -66,8 +57,19 @@ class TangoConan(ConanFile):
         return cmake
 
     def build(self):
+        source_location = os.path.join(self.source_folder, "cppTango")
+        idl_location = os.path.join(self.source_folder, "tango-idl")
+        os.makedirs("tango-idl/include", exist_ok=True)
+        shutil.copy(os.path.join(idl_location, "tango.idl"), os.path.join(self.build_folder, "tango-idl/include/"))
+
+        # conan seems to only support in-source builds right now
+        shutil.copytree(source_location, self.build_folder, ignore=shutil.ignore_patterns(".git"), dirs_exist_ok=True)
+        # Do not require "sed" with this patch
+        tools.patch(patch_file=os.path.join(self.source_folder, NO_SED_PATCH))
+        
         target = "tango" if self.options.shared else "tango-static"
-        self._configured_cmake().build(target=target)
+        cmake = self._configured_cmake()
+        cmake.build(target=target)
 
     def package(self):
         self._configured_cmake().install()
